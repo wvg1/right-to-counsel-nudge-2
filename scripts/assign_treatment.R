4a023e52c82bd4acdb6b0a3762e9ba662598ff04
Will von Geldern <213677874+wvg1@users.noreply.github.com>
2025-11-11 10:00:19 -0800

    added week 6 treatments

--- scripts/assign_treatment.R
+++ scripts/assign_treatment.R
@@ -8,11 +8,11 @@ setwd("C:/Users/wvg1/Documents/right-to-counsel-nudge-2")
 #set seed for reproducibility
 set.seed(651)
 
-#function to assign treatment based on a starting case_ID and number of cases to add
+#function to assign treatment based on a starting household_ID and number of households to add
 generate_assignments <- function(start_id, n, week) {
   
   assignments <- tibble(
-    case_ID = start_id:(start_id + n - 1),
+    household_ID = start_id:(start_id + n - 1),
     treatment = sample(c(0, 1), n, replace = TRUE)
   )
   
@@ -22,7 +22,6 @@ generate_assignments <- function(start_id, n, week) {
   
   return(assignments)
 }
-
 #create week 1 assignments
 generate_assignments(start_id = 1, n = 81, week = 1)
 
@@ -38,3 +37,5 @@ generate_assignments(start_id = 194, n = 76, week = 4)
 #create week 5 assignments
 generate_assignments(start_id = 270, n = 57, week = 5)
 
+#create week 6 assignments
+generate_assignments(start_id = 327, n = 83, week = 6)
