09924d4c487ad5fda6b5c8bd137fb7e256b874aa
Will von Geldern <213677874+wvg1@users.noreply.github.com>
2026-02-24 17:58:07 -0800

    added week 21 assignments

--- scripts/assign_treatment.R
+++ scripts/assign_treatment.R
@@ -81,3 +81,6 @@ generate_assignments(start_id = 1210, n = 75, week = 19)
 
 #create week 20 assignments
 generate_assignments(start_id = 1285, n = 69, week = 20)
+
+#create week 21 assignments
+generate_assignments(start_id = 1354, n = 54, week = 21)
